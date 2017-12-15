1e5e2f17169c83eb8300c2d5c2e10e8744038cd9
Ribamar FS <ola@ribafs.org>
2017-12-15 14:07:00 -0300

    Commit Fernando Pessoa

---
+++ mod_pensamentododia.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* @version $Id:  mod_pensamentos.php, V1.0.0  2009-01-01 $
+* @module Joomla
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+*
+* Adaptado do módulo vivi_code do Serra Antonio (tonio@vivigrosseto.it)
+* http://www.vivigrosseto.it/ 
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class pensamentos
+{
+	function pensamento_aleatorio(){
+		$f_contents = file ('modules/mod_pensamentododia/mod_pensamentododia.txt');
+		srand ((double)microtime()*1000000);
+		$linha_aleatoria = $f_contents[ rand (0, (count ($f_contents) - 1)) ];
+		return $linha_aleatoria;
+	}
+}
+$aleatorio = new pensamentos();
+print '<div id="pensamentos">'.$aleatorio->pensamento_aleatorio().'</div>'; 
+
+?>
+
